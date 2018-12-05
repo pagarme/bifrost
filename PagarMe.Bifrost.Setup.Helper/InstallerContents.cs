d3f8ccf58bacc472a6ff5f5e302d071aa3f398a4
Dara Keon <lc.sales.duarte@gmail.com>
2018-12-05 02:03:45 -0200

    installer: fix output folders

--- PagarMe.Bifrost.Setup.Helper/InstallerContents.cs
+++ PagarMe.Bifrost.Setup.Helper/InstallerContents.cs
@@ -10,16 +10,16 @@ namespace PagarMe.Bifrost.Setup.Helper
     {
         public static void PublishLinux()
         {
-            var updatesPath = getUpdatesPath();
+            var installersPath = getInstallersPath();
 
-            makeLinuxZip(Version, updatesPath);
+            makeLinuxZip(installersPath);
 
-            var jsonPath = Path.Combine(updatesPath, "update-linux.json");
+            var jsonPath = Path.Combine(installersPath, "update-linux.json");
             var json = $@"{{ ""last_version_name"": ""{Version}"" }}";
             File.WriteAllText(jsonPath, json);
         }
 
-        private static void makeLinuxZip(String currentVersion, String updatesPath)
+        private static void makeLinuxZip(String installersPath)
         {
             var originFiles = Path.Combine(MainDir, "bin", "Debug", "Linux");
 
@@ -28,32 +28,35 @@ namespace PagarMe.Bifrost.Setup.Helper
             var exe = Path.Combine(originFiles, "setup.exe");
             File.Delete(exe);
 
-            var zipDestination = Path.Combine(updatesPath, $"bifrost-installer-{currentVersion}.zip");
-            FileExtension.DeleteIfExists(zipDestination);
-            ZipFile.CreateFromDirectory(originFiles, zipDestination, CompressionLevel.Optimal, false);
+            FileExtension.Delete(installersPath, "*.zip");
+
+            var zip = Path.Combine(installersPath, $"bifrost-linux-{Version}.zip");
+            ZipFile.CreateFromDirectory(originFiles, zip, CompressionLevel.Optimal, false);
         }
 
         public static void PublishWindows()
         {
-            var updatesPath = getUpdatesPath();
+            var installersPath = getInstallersPath();
+
+            FileExtension.Delete(installersPath, "*.msi");
 
             var originMsi = Path.Combine(MainDir, "bin", "Debug", "Windows", "BifrostInstaller.msi");
-            var msiDestination = Path.Combine(updatesPath, $"bifrost-installer-{Version}.msi");
-            File.Copy(originMsi, msiDestination, true);
+            var msi = Path.Combine(installersPath, $"bifrost-windows-{Version}.msi");
+            File.Copy(originMsi, msi, true);
 
-            var jsonPath = Path.Combine(updatesPath, "update-windows.json");
+            var jsonPath = Path.Combine(installersPath, "update-windows.json");
             var json = $@"{{ ""last_version_name"": ""{Version}"" }}";
             File.WriteAllText(jsonPath, json);
         }
 
-        private static string getUpdatesPath()
+        private static string getInstallersPath()
         {
-            var updatesPath = Path.Combine(MainDir, "PagarMe.Bifrost.Updates");
+            var installersPath = Path.Combine(MainDir, "installers");
 
-            if (!Directory.Exists(updatesPath))
-                Directory.CreateDirectory(updatesPath);
+            if (!Directory.Exists(installersPath))
+                Directory.CreateDirectory(installersPath);
 
-            return updatesPath;
+            return installersPath;
         }
     }
 }
--- PagarMe.Generic/FileExtension.cs
+++ PagarMe.Generic/FileExtension.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 
 namespace PagarMe.Generic
 {
     public class FileExtension
     {
-        public static void DeleteIfExists(String path)
-        {
-            if (File.Exists(path))
-            {
-                File.Delete(path);
-            }
-        }
+	    public static void DeleteIfExists(String path)
+	    {
+		    if (File.Exists(path))
+		    {
+			    File.Delete(path);
+		    }
+	    }
+
+	    public static void Delete(String path, String pattern)
+	    {
+			Directory.EnumerateFiles(path, pattern)
+				.ToList().ForEach(File.Delete);
+	    }
     }
 }
